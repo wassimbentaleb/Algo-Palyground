958424ca10e5f7d6e112927b893889f11d1abbc4
Wassim Ben Taleb <wassimwbt@gmail.com>
2021-03-10 12:33:16 +0100

    minElement

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 function maxElement() {
     let array = [2, 5, 8, 66, 1, 6, 0]
     console.log('Input: ', array)
+
     let maxValue = array[0]
     for (let i = 0; i < array.length; i++) {
         if (maxValue < array[i])
             maxValue = array[i]
     }
+
     console.log('Output: ', maxValue)
+}
+
+function minElement() {
+    let array = [2, 5, 8, 66, 1, 6, 0]
+    console.log('Input: ', array)
+
+    let minValue = array[0]
+    for (let i = 0; i < array.length; i++) {
+        if (minValue > array[i])
+            minValue = array[i]
+    }
+
+    console.log('Output: ', minValue)
 }
