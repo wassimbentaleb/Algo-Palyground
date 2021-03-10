ce2bb735c41c9d0a2bec9b626d45098d0ccc14c9
Wassim Ben Taleb <wassimwbt@gmail.com>
2021-03-10 13:03:07 +0100

    minmax element

--- main.js
+++ main.js
@@ -23,3 +23,22 @@ function minElement() {
 
     console.log('Output: ', minValue)
 }
+
+function minmaxElement() {
+    let array = [2, 5, 8, 66, 1, 6, 0]
+    console.log('Input: ', array)
+
+    let minValue = array[0]
+    let maxValue = array[0]
+
+    for (let i = 0; i < array.length; i++) {
+        if (minValue > array[i])
+            minValue = array[i]
+
+        if (maxValue < array[i])
+            maxValue = array[i]
+    }
+
+    console.log(`Output: Max = ${maxValue}, Min =${minValue} `)
+
+}
